0d4332517e517430195dd423a107892ed92317e5
Jaremy Creechley <creechley@gmail.com>
2019-08-28 11:51:03 -0600

    update cog

--- package/cog/cog.mk
+++ package/cog/cog.mk
@@ -1,34 +1,34 @@
 ################################################################################
 #
-# cog
+# cog-fdk
 #
 ################################################################################
 
-COG_VERSION = 063df115456a24e464d1e6f284df22a0e65aea8e
-COG_SITE = $(call github,Igalia,cog,$(COG_VERSION))
-COG_DEPENDENCIES = libglib2 libwpe wpewebkit
-COG_LICENSE = MIT
-COG_LICENSE_FILES = COPYING
-COG_CONF_OPTS = \
-	-DCOG_HOME_URI='$(call qstrip,$(BR2_PACKAGE_COG_PROGRAMS_HOME_URI))'
+COG_RDK_VERSION = 063df115456a24e464d1e6f284df22a0e65aea8e
+COG_RDK_SITE = $(call github,Igalia,cog,$(COG_VERSION))
+COG_RDK_DEPENDENCIES = libglib2 libwpe wpewebkit
+COG_RDK_LICENSE = MIT
+COG_RDK_LICENSE_FILES = COPYING
+COG_RDK_CONF_OPTS = \
+	-DCOG_RDK_HOME_URI='$(call qstrip,$(BR2_PACKAGE_COG_PROGRAMS_HOME_URI))'
 
-ifeq ($(BR2_PACKAGE_COG_PLATFORM_FDO),y)
-COG_DEPENDENCIES += libegl libgles wayland wpebackend-fdo
-COG_CONF_OPTS += -DCOG_PLATFORM_FDO=ON
+ifeq ($(BR2_PACKAGE_COG_RDK_PLATFORM_FDO),y)
+COG_RDK_DEPENDENCIES += libegl libgles wayland wpebackend-fdo
+COG_RDK_CONF_OPTS += -DCOG_PLATFORM_FDO=ON
 else
-COG_CONF_OPTS += -DCOG_PLATFORM_FDO=OFF
+COG_RDK_CONF_OPTS += -DCOG_PLATFORM_FDO=OFF
 endif
 
-ifeq ($(BR2_PACKAGE_COG_PROGRAMS),y)
-COG_CONF_OPTS += -DCOG_BUILD_PROGRAMS=ON
+ifeq ($(BR2_PACKAGE_COG_RDK_PROGRAMS),y)
+COG_RDK_CONF_OPTS += -DCOG_BUILD_PROGRAMS=ON
 else
-COG_CONF_OPTS += -DCOG_BUILD_PROGRAMS=OFF
+COG_RDK_CONF_OPTS += -DCOG_BUILD_PROGRAMS=OFF
 endif
 
-ifeq ($(BR2_PACKAGE_COG_PROGRAMS_DBUS_SYSTEM_BUS),y)
-COG_CONF_OPTS += -DCOG_DBUS_SYSTEM_BUS=ON
+ifeq ($(BR2_PACKAGE_COG_RDK_PROGRAMS_DBUS_SYSTEM_BUS),y)
+COG_RDK_CONF_OPTS += -DCOG_DBUS_SYSTEM_BUS=ON
 else
-COG_CONF_OPTS += -DCOG_DBUS_SYSTEM_BUS=OFF
+COG_RDK_CONF_OPTS += -DCOG_DBUS_SYSTEM_BUS=OFF
 endif
 
 $(eval $(cmake-package))
