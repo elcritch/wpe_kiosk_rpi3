9e2a8390a4f4147dcbb0f021f7c34405f27b9bdc
Jaremy Creechley <creechley@gmail.com>
2019-08-28 12:03:12 -0600

    libwpe fix

--- package/libwpe-rdk/libwpe.mk
+++ package/libwpe-rdk/libwpe.mk
@@ -4,16 +4,16 @@
 #
 ################################################################################
 
-LIBWPE_VERSION = 1.0.0
-LIBWPE_SITE = https://wpewebkit.org/releases
-LIBWPE_SOURCE = libwpe-$(LIBWPE_VERSION).tar.xz
-LIBWPE_LICENSE = BSD-2-Clause
-LIBWPE_INSTALL_STAGING = YES
-LIBWPE_DEPENDENCIES = libegl libxkbcommon
+LIBWPE_RDK_VERSION = 1.0.0
+LIBWPE_RDK_SITE = https://wpewebkit.org/releases
+LIBWPE_RDK_SOURCE = libwpe-$(LIBWPE_VERSION).tar.xz
+LIBWPE_RDK_LICENSE = BSD-2-Clause
+LIBWPE_RDK_INSTALL_STAGING = YES
+LIBWPE_RDK_DEPENDENCIES = libegl libxkbcommon
 
 # Workaround for https://github.com/raspberrypi/userland/issues/316
 ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
-LIBWPE_CONF_OPTS += \
+LIBWPE_RDK_CONF_OPTS += \
 	-DCMAKE_C_FLAGS='$(TARGET_CFLAGS) -D_GNU_SOURCE' \
 	-DCMAKE_CXX_FLAGS='$(TARGET_CFLAGS) -D_GNU_SOURCE'
 endif
