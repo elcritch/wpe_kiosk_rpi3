d3e67d11f236a1b181b5f55c2dc6968a0de0188f
Jaremy Creechley <creechley@gmail.com>
2019-10-29 11:50:44 -0600

    pull in fixed post build

--- post-build.sh
+++ post-build.sh
@@ -4,7 +4,7 @@ set -e
 
 ln -rsf $TARGET_DIR/usr/lib/libGLESv2.so $TARGET_DIR/usr/lib/libGLESv2.so.2
 ln -rsf $TARGET_DIR/usr/lib/libEGL.so $TARGET_DIR/usr/lib/libEGL.so.1
-ln -rsf $TARGET_DIR/usr/lib/libWPEBackend-fdo-*.so $TARGET_DIR/usr/lib/libWPEBackend-default.so
+ln -rsf $TARGET_DIR/usr/lib/libWPEBackend-rdk.so $TARGET_DIR/usr/lib/libWPEBackend-default.so
 
 # Create the revert script for manually switching back to the previously
 # active firmware.
@@ -13,3 +13,4 @@ $HOST_DIR/usr/bin/fwup -c -f $NERVES_DEFCONFIG_DIR/fwup-revert.conf -o $TARGET_D
 
 # Copy the fwup includes to the images dir
 cp -rf $NERVES_DEFCONFIG_DIR/fwup_include $BINARIES_DIR
+
